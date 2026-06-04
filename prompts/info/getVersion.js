9cb36d027b3b1bede813b72affa9be13b0e63308
joe <joecelaster2006@gmail.com>
2026-06-05 01:47:53 +0530

    chore:user prompts improved

--- prompts/info/getVersion.js
+++ prompts/info/getVersion.js
@@ -1,17 +1,12 @@
-import inquirer from "inquirer";
-import chalk from "chalk";
-import gradient from "gradient-string";
-import figlet from "figlet";
-import ora from "ora";
-import boxen from "boxen";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
 export function getVersion() {
   try {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    const packageJsonPath = path.join(__dirname, 'package.json');
+    const packageJsonPath = path.join(__dirname, '..', '..', 'package.json');
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
     return packageJson.version;
   } catch (error) {
